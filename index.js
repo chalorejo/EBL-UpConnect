6fc81d5d44e22be2fb7930ee8e6adc4b89e508d7
chalorejo <charissecardineslorejo@gmail.com>
2025-04-15 22:03:56 +0800

    experiment

---
+++ index.js
@@ -0,0 +1,26 @@
+const homeLogo = document.getElementById("homeLogo");
+
+homeLogo.addEventListener("click", function () {
+    window.location.href = "index.html"; 
+});
+
+const up = document.getElementById("up");
+up.addEventListener("click", function () {
+    window.location.href = "index.html"; 
+});
+
+const down = document.getElementById("down");
+down.addEventListener("click", function () {
+    window.location.href = "index.html";
+});
+
+function toggleMenu() {
+    var mobileNav = document.getElementById("mobileNav");
+    if (mobileNav.style.display === "flex") {
+        mobileNav.style.display = "none";
+    } else {
+        mobileNav.style.display = "flex";
+    }
+}
+
+
