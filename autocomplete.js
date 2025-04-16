d614bf2b5285113350181e1b069bbc9476dd94a6
chalorejo <charissecardineslorejo@gmail.com>
2025-04-16 16:49:36 +0800

    improved css

--- autocomplete.js
+++ autocomplete.js
@@ -1,21 +1,21 @@
 let availabeKeyWorlds = [
-    '"Application"',
-    "Dorm",
-    "Dormitory",
-    "Accomodation",
-    "Requirements",
-    "Permit",
-    "Forms",
-    "Overnight",
-    "Request",
-    "Information",
-    "Policy",
-    "Policies",
-    "Responsibilities",
-    "Responsibility",
-    "Reminder",
-    "Reminders",
-    "Offenses"
+    'Application',
+    'Dorm',
+    'Dormitory',
+    'Accomodation',
+    'Requirements',
+    'Permit',
+    'Forms',
+    'Overnight',
+    'Request',
+    'Information',
+    'Policy',
+    'Policies',
+    'Responsibilities',
+    'Responsibility',
+    'Reminder',
+    'Reminders',
+    'Offenses'
 ];
 
 const resultBox = document.querySelector(".result-box");
@@ -24,5 +24,11 @@ const inputBox = document.getElementId("input-box");
 inputBox.onkeyUp = function(){
     let result = [];
     let input = inputBox.value;
-}
 
+    if(input.length){
+        result = availabeKeyWorlds.filter((keyword) => {
+            keyword.toLowerCase(input).includes(input);
+        });
+        console.log(result);
+    }
+}
