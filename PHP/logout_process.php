1c09b6b43cade35cff7703674f8eb04df47d8514
Hannaym <hannacantero5@gmail.com>
2025-05-19 11:21:57 +0800

    Delete PHP directory

--- PHP/logout_process.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // for log out logic
-    session_start();
-    session_unset();     // Clear all session variables
-    session_destroy();   
-
-    header("Location: admin_login.php");
-    exit();
-?>
